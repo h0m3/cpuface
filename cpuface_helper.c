aff1914980a855ba3bb142788590ec4db2e8d0f3
h0m3 <dr.hoome@gmail.com>
2017-07-14 13:51:41 -0300

    Fixed several security bugs
    => Fix several security bugs related to argument parsing to function
    => Check if file exists before editing it, avoid create new file
    
    Thanks to Enzo Matsumiya to all feedbacks and solutions to those bugs.

--- cpuface_helper.c
+++ cpuface_helper.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 // Show help in case of user trying to use this function
 int show_help() {
@@ -15,29 +16,45 @@ int show_help() {
 
 // Append information to a file
 int edit_file(int cpu, char *path, char *text) {
+    if (path == NULL || text == NULL) {
+        printf("Invalid argument parsed to edit_file, please report this error.\n");
+        return 6;
+    }
+
+    if (strlen(path) < 1 || strlen(text) < 1) {
+        printf("Invalid argument parsed to edit_file, please report this error.\n");
+        return 6;
+    }
+
     char *new_path = (char *)malloc(sizeof(char) * (strlen(path) + 3));
     if (sprintf(new_path, path, cpu) < 0) {
         printf("Unable to format string '%s'\n", path);
+        free(new_path);
         return 3;
     }
 
-    FILE *buf = fopen(new_path, "w");
-    if (buf == NULL) {
-        printf("Unable to open '%s' for writing!\n", new_path);
-        free(new_path);
-        return 4;
-    }
+    if (access(new_path, F_OK) != -1) {
+        FILE *buf = fopen(new_path, "w");
+        if (buf == NULL) {
+            printf("Unable to open '%s' for writing!\n", new_path);
+            free(new_path);
+            return 4;
+        }
 
-    if (fprintf(buf, "%s", text) < 0) {
-        printf("Unable to write data (%s) into '%s'\n", text ,new_path);
+        if (fprintf(buf, "%s", text) < 0) {
+            printf("Unable to write data (%s) into '%s'\n", text ,new_path);
+            fclose(buf);
+            free(new_path);
+            return 5;
+        }
+
         fclose(buf);
         free(new_path);
-        return 5;
+        return 0;
+    } else {
+        printf("%s: File not found!\n", new_path);
+        return 7;
     }
-
-    fclose(buf);
-    free(new_path);
-    return 0;
 }
 
 int main(int argc, char *argv[]) {
